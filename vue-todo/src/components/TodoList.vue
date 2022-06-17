6edbe65d5714143a0bb81217fc4e07bf1909a182
areum59 <reddoll8859@gmail.com>
2022-06-17 10:51:26 +0900

    추가된 리스트 내용 삭제하기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <ul>
-            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
                 {{ todoItem }}
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">delete</span>
             </li>
             <!-- 에러가 나는 경우 v-for를 사용할 때 항상 v-bind:key를 사용해야 한다는 이유가 있는데
             vacode에서만 뜨는 에러로 무시해도 됨. (다른 에디터에서는 표시되지 않음) -->
@@ -17,7 +18,13 @@ export default {
             todoItems: []
         }
     },
-
+    methods: {
+        removeTodo: function(todoItem, index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    },
     created: function(){
         if(localStorage.length > 0) {
             for (var i = 0; i < localStorage.length; i++){
@@ -32,5 +39,11 @@ export default {
 </script>
 
 <style scoped>
+ul {list-style: none; padding-left: 0; margin-top: 0; text-align: left;}
+ul li {display: flex; height: 50px; min-height: 50px; line-height: 50px; margin: 0.5rem 0; padding: 0 0.9rem; background-color: #fff; border-radius: 5px;}
 
+.checkBtn {line-height: 45px; color: #62acde; margin-right: 5px;}
+.checkBtnCompleted {color: #b3adad;}
+.textCompleted {text-decoration: line-through; color: #b3adad;}
+.removeBtn {margin-left: auto; color: #de4343; cursor: pointer;}
 </style>
