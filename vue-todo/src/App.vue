14cb38e16089c25de2792becb7cda29515ce4a5c
areum59 <reddoll8859@gmail.com>
2022-06-21 15:53:52 +0900

    Vue.js 중급 강좌 - 리팩토링(할 일 추가 기능)

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <!-- v-on:="" 하위 컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트의 methods 이름" -->
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
+    <!-- v-bind="" 내려보낼 데이터 속성 이름="현재 위치의 컴포넌트" -->
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -15,6 +17,23 @@ import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   name: 'App',
+  data: function(){
+    return{
+      todoItems: []
+    }
+  },
+  methods: {
+    
+  },
+  created: function(){
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
   components: {
     // '컴포넌트 태그명' : 컴포넌트 내용
     'TodoHeader': TodoHeader,
@@ -30,5 +49,5 @@ body {text-align: center; background-color: #f8f8f8;}
 input {border-style: groove; width: 200px;}
 button {border-style: groove;}
 
-.shadow {box-shadow: 5px 10px 10px rgba(0,0,0,0.3);}
+.shadow {box-shadow: 2px px 10px rgba(0,0,0,0.3);}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -3,7 +3,7 @@
         <ul>
             <!-- v-for를 사용할 때 에러가 나는 경우 : 항상 v-bind:key를 사용해야 한다는 이유가 있는데
             vacode에서만 뜨는 에러로 무시해도 됨. (다른 에디터에서는 표시되지 않음) -->
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
                 <!-- check 버튼 -->
                 <span class="checkBtn" v-on:click="toggleComplete(todoItem, index)" 
                     v-bind:class="{checkBtnCompleted: todoItem.completed}">
@@ -26,11 +26,7 @@
 
 <script>
 export default {
-    data: function(){
-        return{
-            todoItems: []
-        }
-    },
+    props: ['propsdata'],
     methods: {
         removeTodo: function(todoItem, index){
             console.log(todoItem, index);
@@ -45,16 +41,6 @@ export default {
             // 즉, 로컬 스토리지의 데이터를 갱신하는 DOM
             localStorage.removeItem(todoItem.item);
             localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-        }
-    },
-    created: function(){
-        if(localStorage.length > 0) {
-            for (var i = 0; i < localStorage.length; i++){
-                if (localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    // .parse() : string을 오브젝트로 변경
-                }
-            }
         }
     }
 };
